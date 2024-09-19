f9cfdbad040dfadae858b2a0523af3359499e1bd
Aquaspot <144318527+Aquaspot@users.noreply.github.com>
2024-09-19 12:03:20 +0200

    Flask installed

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
 import sys
+from flask import Flask
 
+app = Flask(__name__)
 
 sys.path.insert(0, os.path.dirname(__file__))
 
+@app.route('/')
+def index():
+    return 'Hellow World!'
 
-def app.py(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python v' + sys.version.split()[0] + '\n'
-    response = '\n'.join([message, version])
-    return [response.encode()]
+if __name__ == '__main__':
+    app.run()
+
